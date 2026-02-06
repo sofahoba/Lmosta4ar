e42a316bd6cc5eb23964495c6f6c83aca32a8609
sofahoba <youssefehab204@gmail.com>
2026-02-06 20:33:01 +0200

    allow all requests from anywhere

--- src/main/java/com/fullDetailed/fullDetailedDemo/config/SecurityConfig.java
+++ src/main/java/com/fullDetailed/fullDetailedDemo/config/SecurityConfig.java
@@ -71,8 +71,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000","http://127.0.0.1:5500",
-                "http://localhost:5500"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
