47becac16e592ff28d30aa76d28636b1a9ae4930
sofahoba <youssefehab204@gmail.com>
2026-02-09 04:50:44 +0200

    feat : allow caching for admin user management

--- src/main/java/com/fullDetailed/fullDetailedDemo/config/cache/CacheConfig.java
+++ src/main/java/com/fullDetailed/fullDetailedDemo/config/cache/CacheConfig.java
@@ -12,6 +12,6 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("cases");
+        return new ConcurrentMapCacheManager("cases","users_list","case_requests");
     }
 }
