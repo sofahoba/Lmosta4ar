3e5e954164c44c16aa5662a3b4fe115f68add26c
sofahoba <youssefehab204@gmail.com>
2026-05-28 20:35:52 +0200

    update : update ai service base url

--- src/main/java/com/fullDetailed/fullDetailedDemo/config/webClientConfig/WebClientConfing.java
+++ src/main/java/com/fullDetailed/fullDetailedDemo/config/webClientConfig/WebClientConfing.java
@@ -1,5 +1,6 @@
 package com.fullDetailed.fullDetailedDemo.config.webClientConfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,11 +8,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfing {
 
+  @Value("${aiendpoint}")
+  private String aiEndpoint;
+
   @Bean
-  public WebClient webClient(){
-    return WebClient.builder()
-                    .baseUrl("http://host.docker.internal:8000")
-                    .build();
+  public WebClient webClient() {
+      return WebClient.builder()
+          .baseUrl(aiEndpoint)
+          .build();
   }
 
 }
