d1772520367eec7d8388c65a99fc21af6ab1dde1
sofahoba <youssefehab204@gmail.com>
2026-06-20 08:27:09 +0300

    fix : fixed the long time proccess for duplicated invoke and apply custome exception

--- src/main/java/com/fullDetailed/fullDetailedDemo/repository/ModelResultRepository.java
+++ src/main/java/com/fullDetailed/fullDetailedDemo/repository/ModelResultRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ModelResultRepository extends JpaRepository<ModelResult, UUID> {
+
+  boolean existsByCaseEntity_Id(UUID caseId);
+
 }
